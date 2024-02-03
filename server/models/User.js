d789b07ca1ab3f530ba036fd69f6676d5a0a55f2
spencerhenegar-Zwufst <spencerhenegar@gmail.com>
2024-02-02 18:43:29 -0700

    adding user model or at least most of it

---
+++ server/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    bio: {
+        type: String
+    },
+    friends: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    reviews: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Review'
+    }],
+    favoriteGames: [String]
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
