f2a6693377c94a4bd093088719280bdfcb7f2bf5
dylan horyza <horyza.dylan@gmail.com>
2024-02-03 13:09:57 -0700

    setup api in homepage and profilepage

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,4 +1,26 @@
+import React, { useEffect } from 'react';
+
 const ProfilePage = () => {
+  useEffect(() => {
+    const getAllGames = async () => {
+     
+      const apiURL = `https://api.rawg.io/api/games?key=9cdfe8e7af674d6d825da9805c8c6545&dates=2023-08-01,2024-02-01&ordering=-added`;
+
+      try {
+        const response = await fetch(apiURL);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        console.log(data.results);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    getAllGames();
+  }, []);
   return (
     <div>
       <h1>Profile</h1>
