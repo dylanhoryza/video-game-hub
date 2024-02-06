eaa068f819cc3d6e0d50b056e4b126f161b4a8a3
bkness <kbrandon863@gmail.com>
2024-02-05 19:26:59 -0700

    added hash, bcrypt to user model, also added password and email association

--- server/models/User.js
+++ server/models/User.js
@@ -1,34 +1,68 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'Must use a valid email address'],
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        bio: {
+            type: String
+        },
+        avatar: {
+            type: String
+        },
+        followers: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }],
+        reviews: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Review'
+        }],
+        wishlist: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Game'
+        }],
+        currentlyPlaying: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Game'
+        }],
+        favoriteGames: [String]
     },
-    bio: {
-        type: String
-    },
-    avatar: {
-        type: String
-    },
-    followers: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    }],
-    reviews: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Review'
-    }],
-    wishlist: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Game'
-    }],
-    currentlyPlaying: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Game'
-    }],
-    favoriteGames: [String]
+    {
+        toJSON: {
+            virtuals: true,
+        },
+    }
+);
+
+// hash user password
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
 });
+
+// custom method to compare and validate password for logging in
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 
