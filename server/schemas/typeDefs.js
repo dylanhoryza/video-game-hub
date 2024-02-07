b799fa4e9a24a2e63b23e49e4de4f7c3be2c4b7a
bkness <kbrandon863@gmail.com>
2024-02-07 13:26:29 -0700

    added typeDefs and resolver logic for post update and delete Post, sandbox testing successful

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -33,6 +33,11 @@ type Game {
 
   input AddToWishlistInput {
     gameId: ID!
+    name: String!
+    image: String
+    platforms: [String]
+    rating: Float
+    releaseDate: String
     
   }
   
@@ -58,7 +63,7 @@ type Mutation {
     createPost(title: String!, content: String!, authorId: ID!): Post!
     deletePost(postId: ID!): Post
     updatePost(postId: ID!, content: String!): Post!
-  }
-`;
+}
 
+`;
 module.exports = typeDefs;
