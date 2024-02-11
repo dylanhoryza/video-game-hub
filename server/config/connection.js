53dfb31b95f13832c8d4d79f5889abe63b3aa6fa
dylan horyza <horyza.dylan@gmail.com>
2024-02-11 11:51:14 -0700

    fixed latest pull erverything should work

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,9 @@
 const { connect, connection } = require('mongoose');
 const mongoose = require('mongoose');
  
- mongoose.connect(
-     process.env.MONGODB_URI || 'mongodb+srv://bkness:ziggy@cluster0.pf0alen.mongodb.net/game_test_db'
-   );
+//  mongoose.connect(
+//      process.env.MONGODB_URI || 'mongodb+srv://bkness:ziggy@cluster0.pf0alen.mongodb.net/game_test_db'
+//    );
 
 
 mongoose.connect(
